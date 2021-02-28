ac8093d3551dd14cbd5fe3a0f06ee32ac3919e81
sooraj713 <62977115+sooraj713@users.noreply.github.com>
2021-03-01 01:20:14 +0530

    LOGIN SCRIPT CHANGES DONE

--- js/login.js
+++ js/login.js
@@ -17,29 +17,41 @@ function isAuthenticatedUser(){
 	let u = JSON.parse(localStorage.getItem('users'));
 	if (u.length == 0) {
 		let label = document.getElementById("loginmsg");
-		label.innerHTML = `* Please enter valid username and password *`;
+		label.innerHTML = `Please enter valid username and password...!`;
 		label.style.color="red";
 		label.style.display="block";	
 	}
 	else{
-		for (var i = 0; i < u.length; i++) {
+		let flag = 0;
+		let label = document.getElementById("loginmsg");
+		for (let i = 0; i < u.length; i++) {
 			if(u[i].userName == uname && u[i].password == password)
 			{
-				console.log("Login Successfully");
+				label.textContent = "You have Logged in Successfully";
+				label.style.color = "green";
+				label.style.display = "block";
 				localStorage.setItem('activeUser',uname);
-				document.getElementById('login').href = "profile.html";
-				break;
+				setTimeout(function(){
+		            window.location.href = 'profile.html';
+		         }, 1000);
+				flag=1;
+				break;	
 			}
-			else
-			{
-			let label = document.getElementById("loginmsg");
-			label.innerHTML = `* Please enter valid username and password *`;
-			label.style.color="red";
-			label.style.display="block";
-			}
+		}
+		if (flag == 0)
+		{
+		label.innerHTML = `* Please enter valid username and password *`;
+		label.style.color="red";
+		label.style.display="block";
 		}
 	}
 }//login authentication function ends here..........................
 
 //this is for showing notification count ................................
 document.getElementById("notification").innerHTML = Number(localStorage.getItem('notifications'));
+
+//function for stop the refreshing page after submitting form data
+
+function onSubmit (e) {
+  e.preventDefault();
+}
